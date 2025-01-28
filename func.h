2fbb09985ec2e2bd8e7f75dc759970afec74057f
Wooyoung Jang <wooyoung.jang@uta.edu>
2025-01-28 12:36:44 -0600

    rename and file modularization

---
+++ func.h
@@ -0,0 +1,5 @@
+double myroot_diff_xsec(double* x, double* param);
+double diff_xsec(double ene_dm, double ene_r, double m_dm);
+double xsec_analytic_integration(double ene_dm, double m_dm);
+double xsec_numerical_integration(double ene_dm, double m_dm);
+
