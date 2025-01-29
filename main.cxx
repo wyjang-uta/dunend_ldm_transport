9d5f8dbc51973045cc85ee4003f310e0b422a122
wyjang-uta <wooyoung.jang@uta.edu>
2025-01-28 22:54:35 -0600

    moller-trumbore algorithm implemented

--- main.cxx
+++ main.cxx
@@ -10,6 +10,8 @@
 
 #include "physconst.h"
 #include "func.h"
+#include "Vector3D.h"
+#include "RayIntersect.h"
 
 /// input constants (data card)
 /// physical variables
@@ -29,21 +31,44 @@ const double e_0 = 2.0 * energy_dm * energy_dm * m_e / ( 2.0 * energy_dm * m_e +
 int main(int argc, char* argv[])
 {
   double mA = dm_ratio * mass_dm;
-  std::cout << "differential cross section with inputs: " << std::endl;
-  std::cout << "Dark matter energy = " << energy_dm << " (MeV)" << std::endl;
-  std::cout << "Dark matter mass = " << mass_dm << " (MeV)" << std::endl;
-  std::cout << "Recoiled electron energy = " << energy_recoil << " (MeV)" << std::endl;
-  std::cout << "Detector threshold = " << e_thr << " (MeV)" << std::endl;
-  std::cout << "result (myfunc) = " << diff_xsec(energy_dm, energy_recoil, mass_dm, mA) << std::endl;
+  //std::cout << "differential cross section with inputs: " << std::endl;
+  //std::cout << "Dark matter energy = " << energy_dm << " (MeV)" << std::endl;
+  //std::cout << "Dark matter mass = " << mass_dm << " (MeV)" << std::endl;
+  //std::cout << "Recoiled electron energy = " << energy_recoil << " (MeV)" << std::endl;
+  //std::cout << "Detector threshold = " << e_thr << " (MeV)" << std::endl;
+  //std::cout << "result (myfunc) = " << diff_xsec(energy_dm, energy_recoil, mass_dm, mA) << std::endl;
   //std::cout << "result (TF1) = " << fXsec->Eval(energy_recoil) << std::endl;
 
-  double x = energy_recoil;
-  double par[3] = {energy_dm, mass_dm, mA};
-  std::cout << "result (myroot_diff_xsec) = " << myroot_diff_xsec(&x, par) << std::endl;
-  double e_0 = 2.0 * energy_dm * energy_dm * m_e / ( 2.0 * energy_dm * m_e + mass_dm * mass_dm );
-  std::cout << "The zero of the differential cross section is = " << e_0 << std::endl;
-  std::cout << "Total cross section (analytic)= " << xsec_analytic_integration(energy_dm, mass_dm, mA, e_thr, e_0) << std::endl;
-  std::cout << "Total cross section (numerical integration)= " << xsec_numerical_integration(energy_dm, mass_dm, mA, e_thr, e_0) << std::endl;
+  //double x = energy_recoil;
+  //double par[3] = {energy_dm, mass_dm, mA};
+  //std::cout << "result (myroot_diff_xsec) = " << myroot_diff_xsec(&x, par) << std::endl;
+  //double e_0 = 2.0 * energy_dm * energy_dm * m_e / ( 2.0 * energy_dm * m_e + mass_dm * mass_dm );
+  //std::cout << "The zero of the differential cross section is = " << e_0 << std::endl;
+  //std::cout << "Total cross section (analytic)= " << xsec_analytic_integration(energy_dm, mass_dm, mA, e_thr, e_0) << std::endl;
+  //std::cout << "Total cross section (numerical integration)= " << xsec_numerical_integration(energy_dm, mass_dm, mA, e_thr, e_0) << std::endl;
+
+  // Load Geometry
+  Vector3D v0(nd_location_vertex[0][0], nd_location_vertex[0][1], nd_location_vertex[0][2]);
+  Vector3D v1(nd_location_vertex[1][0], nd_location_vertex[1][1], nd_location_vertex[1][2]);
+  Vector3D v2(nd_location_vertex[2][0], nd_location_vertex[2][1], nd_location_vertex[2][2]);
+  Vector3D v3(nd_location_vertex[3][0], nd_location_vertex[3][1], nd_location_vertex[3][2]);
+  Vector3D v4(nd_location_vertex[4][0], nd_location_vertex[4][1], nd_location_vertex[4][2]);
+  Vector3D v5(nd_location_vertex[5][0], nd_location_vertex[5][1], nd_location_vertex[5][2]);
+  Vector3D v6(nd_location_vertex[6][0], nd_location_vertex[6][1], nd_location_vertex[6][2]);
+  Vector3D v7(nd_location_vertex[7][0], nd_location_vertex[7][1], nd_location_vertex[7][2]);
+  Box box(v0, v1, v2, v3, v4, v5, v6, v7);
+
+  // Print Geometry
+  std::cout << "nd_vertex 1: (" << nd_location_vertex[0][0] << ", " << nd_location_vertex[0][1] << ", " << nd_location_vertex[0][2] << ").\n";
+  std::cout << "nd_vertex 2: (" << nd_location_vertex[1][0] << ", " << nd_location_vertex[1][1] << ", " << nd_location_vertex[1][2] << ").\n";
+  std::cout << "nd_vertex 3: (" << nd_location_vertex[2][0] << ", " << nd_location_vertex[2][1] << ", " << nd_location_vertex[2][2] << ").\n";
+  std::cout << "nd_vertex 4: (" << nd_location_vertex[3][0] << ", " << nd_location_vertex[3][1] << ", " << nd_location_vertex[3][2] << ").\n";
+  std::cout << "nd_vertex 5: (" << nd_location_vertex[4][0] << ", " << nd_location_vertex[4][1] << ", " << nd_location_vertex[4][2] << ").\n";
+  std::cout << "nd_vertex 6: (" << nd_location_vertex[5][0] << ", " << nd_location_vertex[5][1] << ", " << nd_location_vertex[5][2] << ").\n";
+  std::cout << "nd_vertex 7: (" << nd_location_vertex[6][0] << ", " << nd_location_vertex[6][1] << ", " << nd_location_vertex[6][2] << ").\n";
+  std::cout << "nd_vertex 8: (" << nd_location_vertex[7][0] << ", " << nd_location_vertex[7][1] << ", " << nd_location_vertex[7][2] << ").\n";
+
+
 
   // Loading pi0 file
   // - build 4-vector container
@@ -98,12 +123,16 @@ int main(int argc, char* argv[])
   masses[0] = mass_dm;
   masses[1] = mass_dm;
   long decayCount_phi = 0;
+  long decayCount_phi_accepted = 0;
   // initialize random seed
   TRandom3 seed_darkPhoton_decay(1);
   std::vector<TLorentzVector> darkMatters;
   TLorentzVector* dm_particle1;
   TLorentzVector* dm_particle2;
   TH1D* hphi = new TH1D("hphi", "dark matter flux", 1200, 0, 120000);
+  Vector3D rayO(0.0, 0.0, 0.0);
+  Vector3D rayV(0.0, 0.0, 0.0);
+  bool interCube;
   for( auto& darkPhoton_index : darkPhotons )
   {
     decay.SetDecay(darkPhoton_index, 2, masses, "");
@@ -112,14 +141,32 @@ int main(int argc, char* argv[])
     dm_particle2 = decay.GetDecay(1);
     // TODO: determine each particle is heading toward ND fiducial volume
     //
+    rayV.x = dm_particle1->Px();
+    rayV.y = dm_particle1->Py();
+    rayV.z = dm_particle1->Pz();
+
+    interCube = RayIntersectsCube(rayO, rayV, &box);
+    if( interCube )
+    {
+      darkMatters.push_back(*dm_particle1);
+      hphi->Fill(dm_particle1->E());
+      decayCount_phi_accepted++;
+    }
+    rayV.x = dm_particle2->Px();
+    rayV.y = dm_particle2->Py();
+    rayV.z = dm_particle2->Pz();
+    interCube = RayIntersectsCube(rayO, rayV, &box);
+    if( interCube )
+    {
+      darkMatters.push_back(*dm_particle2);
+      hphi->Fill(dm_particle2->E());
+      decayCount_phi_accepted++;
+    }
     //
-    darkMatters.push_back(*dm_particle1);
-    darkMatters.push_back(*dm_particle2);
-    hphi->Fill(dm_particle1->E());
-    hphi->Fill(dm_particle2->E());
     decayCount_phi++;
   }
   std::cout << decayCount_phi << " x 2 light dark mater particles created by TGenPhaseSpace code.\n";
+  std::cout << decayCount_phi_accepted << " light dark mater particles accepted within DUNE ND fiducial volume.\n";
 
 
   TFile fOutput("proto_output.root","RECREATE");
--- physconst.h
+++ physconst.h
@@ -23,14 +23,16 @@ const double fiducial_x               = 6.0;
 const double fiducial_y               = 2.0;
 const double fiducial_z               = 3.0;
 
-const double fiducial_vertex[0][3] = { 0.5 * fiducial_x,  0.5 * fiducial_y, -0.5 * fiducial_z};
-const double fiducial_vertex[1][3] = {-0.5 * fiducial_x,  0.5 * fiducial_y, -0.5 * fiducial_z};
-const double fiducial_vertex[2][3] = { 0.5 * fiducial_x, -0.5 * fiducial_y, -0.5 * fiducial_z};
-const double fiducial_vertex[3][3] = {-0.5 * fiducial_x, -0.5 * fiducial_y, -0.5 * fiducial_z};
-const double fiducial_vertex[4][3] = { 0.5 * fiducial_x,  0.5 * fiducial_y,  0.5 * fiducial_z};
-const double fiducial_vertex[5][3] = {-0.5 * fiducial_x,  0.5 * fiducial_y,  0.5 * fiducial_z};
-const double fiducial_vertex[6][3] = { 0.5 * fiducial_x, -0.5 * fiducial_y,  0.5 * fiducial_z};
-const double fiducial_vertex[7][3] = {-0.5 * fiducial_x, -0.5 * fiducial_y,  0.5 * fiducial_z};
+const double fiducial_vertex[8][3] = {
+  { 0.5 * fiducial_x,  0.5 * fiducial_y, -0.5 * fiducial_z},
+  {-0.5 * fiducial_x,  0.5 * fiducial_y, -0.5 * fiducial_z},
+  { 0.5 * fiducial_x, -0.5 * fiducial_y, -0.5 * fiducial_z},
+  {-0.5 * fiducial_x, -0.5 * fiducial_y, -0.5 * fiducial_z},
+  { 0.5 * fiducial_x,  0.5 * fiducial_y,  0.5 * fiducial_z},
+  {-0.5 * fiducial_x,  0.5 * fiducial_y,  0.5 * fiducial_z},
+  { 0.5 * fiducial_x, -0.5 * fiducial_y,  0.5 * fiducial_z},
+  {-0.5 * fiducial_x, -0.5 * fiducial_y,  0.5 * fiducial_z}
+};
 
 const double dist_mc0_nd              = 574.0;
 const double dist_ground_nd           = 62.50;
@@ -38,6 +40,25 @@ const double dune_nd_rotation_angle_in_x = asin(dist_ground_nd/dist_mc0_nd);
 
 const double cosine = cos(dune_nd_rotation_angle_in_x);
 const double sine   = sin(dune_nd_rotation_angle_in_x);
-const double rotated_fiducial_vertex[0][3] = { fiducial_vertex[0][0], cosine * fiducial_vertex[0][1] - sine * fiducial_vertex[0][2], sine * fiducial_vertex[0][1] + cosine * fiducial_vertex[0][2]};
-const double rotated_fiducial_vertex[1][3] = { fiducial_vertex[1][0], cosine * fiducial_vertex[1][1] - sine * fiducial_vertex[1][2], sine * fiducial_vertex[1][1] + cosine * fiducial_vertex[1][2]};
+const double rotated_fiducial_vertex[8][3] = {
+  { fiducial_vertex[0][0], cosine * fiducial_vertex[0][1] - sine * fiducial_vertex[0][2], sine * fiducial_vertex[0][1] + cosine * fiducial_vertex[0][2]},
+  { fiducial_vertex[1][0], cosine * fiducial_vertex[1][1] - sine * fiducial_vertex[1][2], sine * fiducial_vertex[1][1] + cosine * fiducial_vertex[1][2]},
+  { fiducial_vertex[2][0], cosine * fiducial_vertex[2][1] - sine * fiducial_vertex[2][2], sine * fiducial_vertex[2][1] + cosine * fiducial_vertex[2][2]},
+  { fiducial_vertex[3][0], cosine * fiducial_vertex[3][1] - sine * fiducial_vertex[3][2], sine * fiducial_vertex[3][1] + cosine * fiducial_vertex[3][2]},
+  { fiducial_vertex[4][0], cosine * fiducial_vertex[4][1] - sine * fiducial_vertex[4][2], sine * fiducial_vertex[4][1] + cosine * fiducial_vertex[4][2]},
+  { fiducial_vertex[5][0], cosine * fiducial_vertex[5][1] - sine * fiducial_vertex[5][2], sine * fiducial_vertex[5][1] + cosine * fiducial_vertex[5][2]},
+  { fiducial_vertex[6][0], cosine * fiducial_vertex[6][1] - sine * fiducial_vertex[6][2], sine * fiducial_vertex[6][1] + cosine * fiducial_vertex[6][2]},
+  { fiducial_vertex[7][0], cosine * fiducial_vertex[7][1] - sine * fiducial_vertex[7][2], sine * fiducial_vertex[7][1] + cosine * fiducial_vertex[7][2]}
+};
+
+const double nd_location_vertex[8][3] = {
+  { rotated_fiducial_vertex[0][0], rotated_fiducial_vertex[0][1], rotated_fiducial_vertex[0][2] + dist_mc0_nd },
+  { rotated_fiducial_vertex[1][0], rotated_fiducial_vertex[1][1], rotated_fiducial_vertex[1][2] + dist_mc0_nd },
+  { rotated_fiducial_vertex[2][0], rotated_fiducial_vertex[2][1], rotated_fiducial_vertex[2][2] + dist_mc0_nd },
+  { rotated_fiducial_vertex[3][0], rotated_fiducial_vertex[3][1], rotated_fiducial_vertex[3][2] + dist_mc0_nd },
+  { rotated_fiducial_vertex[4][0], rotated_fiducial_vertex[4][1], rotated_fiducial_vertex[4][2] + dist_mc0_nd },
+  { rotated_fiducial_vertex[5][0], rotated_fiducial_vertex[5][1], rotated_fiducial_vertex[5][2] + dist_mc0_nd },
+  { rotated_fiducial_vertex[6][0], rotated_fiducial_vertex[6][1], rotated_fiducial_vertex[6][2] + dist_mc0_nd },
+  { rotated_fiducial_vertex[7][0], rotated_fiducial_vertex[7][1], rotated_fiducial_vertex[7][2] + dist_mc0_nd }
+};
 
